c24f9c9c2bf84b15927c50baba2e48bf139d813d
Vladyslav <khomenko.vlad7@gmail.com>
2019-10-08 18:57:20 +0300

    Code refactor

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,11 @@
 from Crypto.Cipher import AES
 from base64 import b64decode
+
 key = b64decode('f8WwucPW1fxcJ/sb65X2Dz9qkd4xrOZmKdmyIgOTtRM=')
 
-crypted_list = [b64decode('AQIvZ0rae6csEKp09v5UvJfWyehtBM3rtPHsRJIPVxHyyaZGy/v9xFnGv18='),
-                b64decode('AQIn08cVW4SAPsqT4VdX24WeRh+WO/4lrMbh436HMSYRv0q09P0='),
-                b64decode('''AQI5CFY7+Sq8DGq7XYD7X8EyXZcUOjWka2WETTgK1zOu6oAOwE1cParQ4KvMyV60FoH2AcJOErQ6
+crypted_list = ['AQIvZ0rae6csEKp09v5UvJfWyehtBM3rtPHsRJIPVxHyyaZGy/v9xFnGv18=',
+                'AQIn08cVW4SAPsqT4VdX24WeRh+WO/4lrMbh436HMSYRv0q09P0=',
+                '''AQI5CFY7+Sq8DGq7XYD7X8EyXZcUOjWka2WETTgK1zOu6oAOwE1cParQ4KvMyV60FoH2AcJOErQ6
 r+0ZqeHuoMo2igaug7cKEhOj0iFMrwE6Ad4yjoq582P0OQy5upMBia86SIRLqDNZlniFNWJUUJdk
 +a+FaV7jrtXzgtuLU838YkUCXV+NihesneXXlrPEJCb74SmfxUh2VROdip/uy9k+k6qXWFB6+Cnn
 uY11sZhlCiWb4ofccD7Iz7JdnT+cFOOIbAh0UjBVYwqijae3WfenUSgFzwX/V20E3vYAo+T+aF2I
@@ -19,9 +20,9 @@ UUqdeSUxGNq8eENAfjlzf2dxYFTg0659lmM/t/UW8dVbgu9ZkleAmyfktmC42aCKiczqtWx62lKF
 nZcJHoQu1eqnDvCDZAP4amFFYiYGTdqDoTGqv9TSAcsVQEWYUiCfXQGzZq0+P/IGgeV+ACLscpF4
 PyJe1KhBOHZRny9n3a6dprblFs65TKWVSnWiXui50A4NlUZyQKUpuDD3yXukwFQ5fGYxnuE0ZcDl
 Pem/JvAFVYRussyDceEeLgCwEm/g+r2Riw/8RteFRNA3B7NngctP5wcu106AqVPP6PrnIbMLRlkQ
-gQhr2zKXp2Mqx6mgREWIhfkpPEDoJuGEEHOR'''),
-                b64decode('AQL+SIyCOQTbwB84By9oa1NNruzfEO3xQMx/Qj5IQBWbPQ+w83nDf3vI1qo6CVvG9+ikvN4L3CcdrKq5lPnT1bE=')]
+gQhr2zKXp2Mqx6mgREWIhfkpPEDoJuGEEHOR''',
+                'AQL+SIyCOQTbwB84By9oa1NNruzfEO3xQMx/Qj5IQBWbPQ+w83nDf3vI1qo6CVvG9+ikvN4L3CcdrKq5lPnT1bE=']
 
 for crypted_text in crypted_list:
-    a = AES.new(key, AES.MODE_GCM, crypted_text[2:14])
-    print(a.decrypt(crypted_text[14:]))
+    a = AES.new(key, AES.MODE_GCM, b64decode(crypted_text)[2:14])
+    print(a.decrypt(b64decode(crypted_text)[14:]))
